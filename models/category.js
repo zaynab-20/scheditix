220355f1e71faf343b74d83210dbfb80d9dec457
zaynab-20 <zatoloye@gmail.com>
2025-04-20 10:25:57 +0100

    commit

--- models/category.js
+++ models/category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const eventCategorySchema = new mongoose.Schema({
     categoryName:{
       type:String,
-      enum:['Wedding','Tech Event','Birthday Party','Naming Ceremony','Comedy','Get Together'],
+      enum:['Wedding','Tech Event','Birthday Party','Fashion Show','Comedy','Confrence'],
       unique: true
     },
     events:[
